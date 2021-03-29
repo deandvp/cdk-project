e06af280867221cd77fcc446b5afddb6e614781e
Dean Varghese Plackal <dean.dvp@gmail.com>
2021-03-29 19:50:39 +0530

    Create VPC with 2 subnets

--- app.py
+++ app.py
@@ -11,24 +11,12 @@ from aws_cdk import core
 
 from cdk.cdk_stack import CdkStack
 
-
+from resource_stack_dvp.custom_vpc import ResourcesStackDVP
 app = core.App()
-CdkStack(app, "CdkStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+CdkStack(app, "CdkStack")
 
-    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+ResourcesStackDVP(app, "dvp-vpc-stack")
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=core.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
 
 app.synth()
